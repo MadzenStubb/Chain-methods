01b8f698ecb6729db424e3444a8262b853eaaa82
MadzenStubb <qinglorg@gmail.com>
2023-11-27 10:08:22 -0300

    Array Methods

--- script3.js
+++ script3.js
@@ -1,6 +1,18 @@
-let chain = "Hola rancio como estas?";
+let chain = ["Mores", "Vegaz", "Armando", "Brendan"];
 // let chain2 = "";
 
-let result = chain.split("como");
+document.write(chain + "<br><br>");
+
+// let result = chain.reverse();
+// chain.unshift()
+// chain.shift()
+// chain.pop()
+// chain.sort()
+// let result = chain.indexOf("Brendan");
+// document.write(result + "<br><br>");
+
+// let result = chain.filter( chain => document.write(chain + "<br>"))
+// let result = chain.forEach( chain => document.write(chain + "<br>"))
+let result = chain.filter(chain => chain.length > 7)
 
 document.write(result);
